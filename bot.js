531d18b4fb63d62252bd536e6bb085b6ba03fbc6
Jeremy Leitmeyer <jeremyleitmeyer@gmail.com>
2017-12-15 18:34:21 -0500

    test

--- bot.js
+++ bot.js
@@ -5,12 +5,13 @@ var Client = require('node-rest-client').Client;
 var key = require('./api.json');
 var client = new Client();
 var osuapi = require('osu-api');
+var app = require('express')();
 var osu = new osuapi.Api(key);
 var np, data, response;
 
-var osuUser = "7653158"
+var osuUser = "user"
 
-var n = "https://osu.ppy.sh/api/get_user_recent?u=" + osuUser + "&k=" + key.token + "&limit=1"
+var apiOne = "https://osu.ppy.sh/api/get_user_recent?u=" + osuUser + "&k=" + key.token + "&limit=1"
 
 //logger settings
 logger.remove(logger.transports.Console);
@@ -32,7 +33,11 @@ bot.on('ready', function (evt) {
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
 
-bot.on('message', function (user, userID, channelID, message, evt) {
+
+
+app.post('/np-chan', function(req, res){
+	var np = res.user
+	bot.on('message', function (user, userID, channelID, message, evt) {
   // will listen for messages that will start with `!`
   if (message.substring(0, 1) == '!') {
       var args = message.substring(1).split(' ');
@@ -42,20 +47,13 @@ bot.on('message', function (user, userID, channelID, message, evt) {
       switch(cmd) {
       //commands
       case 'np':
-			client.get(n, function (data, response) {
-				// parsed response body as js object
-				np = data[0].beatmap_id
-			var b = "https://osu.ppy.sh/api/get_beatmaps?b=" + np + "&k=" + key.token + "&limit=1"
-			   	client.get(b, function (data, response){
-			    	title = data[0].title
-			    	artist = data[0].artist
-              bot.sendMessage({
-                to: channelID,
-                message: "Listen to this! " + title + " by: " + artist 
-              });
-          	});
-			   	});	
-      break;
-    }
-  }
+        bot.sendMessage({
+          to: channelID,
+          message: "You are " + np
+        });
+	      break;
+	    }
+	  }
+	})
 })
+
