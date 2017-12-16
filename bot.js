be48dda804164da8ee6de69b2a9ac0ea6ba88616
Jeremy Leitmeyer <jeremyleitmeyer@gmail.com>
2017-12-15 23:05:50 -0500

    1.0

--- bot.js
+++ bot.js
@@ -4,6 +4,8 @@ const auth = require('./auth.json')
 const request = require('request-promise');
 const bodyParser = require('body-parser');
 const app = require('express')();
+const Client = require('node-rest-client').Client;
+const client = new Client();
 const channelID = '380442081103183876';
 
 app.use(bodyParser.json());
@@ -40,24 +42,38 @@ app.get('/np-chan', function(req,res){
 app.post('/np-chan', function(req, res){    // your JSON
 	console.log(req.body)
 
-  bot.sendMessage({
-    to: channelID,
-    message: '**' + req.body.user + '**' + ' is listening to:',
-    embed: {
-    color: 1139500,
-    footer: { 
-      text: '© Riker, Flo, & Tux'
-    },
-    thumbnail:
-    {
-      url: 'https://i.imgur.com/wL1Q2Sk.png'
-    },
-    title: '**' + req.body.artist + '-' + req.body.title,
-    url: req.body.url ,
-    description: 'things will go here',
-  }
-  	
-  });
+	var beatmap = req.body.url.split("/").pop()
+
+	var osu = "https://osu.ppy.sh/api/get_beatmaps?b=" + beatmap + "&k=" + auth.osu + "&limit=1"
+	client.get(osu, function (data, response){
+
+		var length = data[0].total_length.split('')
+		if (length.length === 4){
+			length.splice(2, 0, ':')
+		} else if (length.length === 3) {
+			length.splice(1, 0, ':')
+		} else {
+			length.splice(0, 0, ':')
+		}
+	  bot.sendMessage({
+	    to: channelID,
+	    message: '**' + req.body.user + '**' + ' is listening to:',
+	    embed: {
+	    color: 1139500,
+	    footer: { 
+	      text: '© Riker, Flo, & Tux'
+	    },
+	    thumbnail:
+	    {
+	      url: 'https://i.imgur.com/wL1Q2Sk.png',
+	      height: 100
+	    },
+	    title: req.body.artist + ' - ' + req.body.title,
+	    description: 'BPM: ' + data[0].bpm + '\nLength: ' + length.join('') + '\nBeatmap: [Download](' + req.body.url + ')'
+	  }
+	  	
+	  });
+	})
   res.send(req.body)
 })
 
